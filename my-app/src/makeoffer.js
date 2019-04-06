b5f5e7f1616739052c2308998b08bc0d4f09dfb7
chrisn3 <35389960+chrisn3@users.noreply.github.com>
2019-04-05 22:42:29 -1000

    Added search function mockup

--- my-app/src/makeoffer.js
+++ my-app/src/makeoffer.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
+import { Container, Dropdown, Menu } from 'semantic-ui-react';
 
-class Makeoffer extends React.Component {
+const itemOptions = [
+  { key: 'ho', value: 'ho', text: 'Household items' },
+  { key: 'sc', value: 'sc', text: 'School Supplies' },
+  { key: 'fu', value: 'fu', text: 'Furniture' },
+  { key: 'te', value: 'te', text: 'Textbooks' },
+  { key: 'mi', value: 'mi', text: 'Miscellaneous' },
+]
 
+class MiddleMenu extends React.Component {
   render() {
     return (
-        <h1>Make Offer</h1>
-    );
+
+        <Menu className="ui borderless middlemenu menu">
+          <Container className="ui centered grid container">
+            <Dropdown
+                placeholder='Select Category of item'
+                fluid
+                search
+                selection
+                options={itemOptions}
+            />
+          </Container>
+        </Menu>
+    )
   }
 }
 
-export default Makeoffer
+
+class MakeOffer extends React.Component {
+
+  render() {
+    return (
+        <div>
+          <MiddleMenu/>
+        </div>
+    );
+  }
+}
+export default MakeOffer;
