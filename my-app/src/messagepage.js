1025308a0402b8ba7f20aa6378060d10d0797d8e
Katherine Piniol <piniolk@hawaii.edu>
2019-04-08 10:28:41 -1000

    wip issue-13

---
+++ my-app/src/messagepage.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Comment, Header } from 'semantic-ui-react';
+
+class Message extends React.Component {
+  render() {
+    return (
+        <Comment.Group>
+          <Header as='h3' dividing>
+            Comments
+          </Header>
+
+          <Comment>
+            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
+            <Comment.Content>
+              <Comment.Author as='a'>Matt</Comment.Author>
+              <Comment.Metadata>
+                <div>Today at 5:42PM</div>
+              </Comment.Metadata>
+              <Comment.Text>How artistic!</Comment.Text>
+              <Comment.Actions>
+                <Comment.Action>Reply</Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+
+          <Comment>
+            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+            <Comment.Content>
+              <Comment.Author as='a'>Elliot Fu</Comment.Author>
+              <Comment.Metadata>
+                <div>Yesterday at 12:30AM</div>
+              </Comment.Metadata>
+              <Comment.Text>
+                <p>This has been very useful for my research. Thanks as well!</p>
+              </Comment.Text>
+              <Comment.Actions>
+                <Comment.Action>Reply</Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+            <Comment.Group>
+              <Comment>
+                <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
+                <Comment.Content>
+                  <Comment.Author as='a'>Jenny Hess</Comment.Author>
+                  <Comment.Metadata>
+                    <div>Just now</div>
+                  </Comment.Metadata>
+                  <Comment.Text>Elliot you are always so right :)</Comment.Text>
+                  <Comment.Actions>
+                    <Comment.Action>Reply</Comment.Action>
+                  </Comment.Actions>
+                </Comment.Content>
+              </Comment>
+            </Comment.Group>
+          </Comment>
+
+          <Comment>
+            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+            <Comment.Content>
+              <Comment.Author as='a'>Joe Henderson</Comment.Author>
+              <Comment.Metadata>
+                <div>5 days ago</div>
+              </Comment.Metadata>
+              <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
+              <Comment.Actions>
+                <Comment.Action>Reply</Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+
+          <Form reply>
+            <Form.TextArea />
+            <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+          </Form>
+        </Comment.Group>
+    )
+    )
+  }
+}
+
+class Messagepage extends React.Component {
+
+  render() {
+    return (
+        <h1>Login</h1>
+        <Message/>
+    );
+  }
+}
+
+export default Messagepage
