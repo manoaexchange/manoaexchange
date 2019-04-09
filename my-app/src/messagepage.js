996dfaa06e53cf8e8e88ceb6c272a851337c793a
Katherine Piniol <piniolk@hawaii.edu>
2019-04-08 16:15:28 -1000

    improving message feed

--- my-app/src/messagepage.js
+++ my-app/src/messagepage.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Card, Container, Image } from 'semantic-ui-react';
-import piniolkimg from 'piniolkimg.png';
+import { Card, Container, Image, Grid, Divider, Segment, Form, Button } from 'semantic-ui-react';
+import piniolkimg from './piniolkimg.png';
 
 class MessageHead extends React.Component {
   render() {
+    const msgTitleMargin = { margin: "10px" };
     return (
         <div>
-          <h1>Messages</h1>
-          <h2>Katherine Piniol</h2>
-          <Image src={piniolkimg} size='tiny'/>
+          <Grid centered style={msgTitleMargin}>
+            <h2>Messages from Katherine Piniol</h2>
+          </Grid>
+          <Image src={piniolkimg} size='tiny' centered/>
+          <Divider />
         </div>
     )
   }
@@ -19,26 +22,66 @@ class MessageHead extends React.Component {
 class MessageFeed extends React.Component {
   render() {
     return (
-        <Container>
-          <div>
-            <Card.Group>
-              <Card>
-                <Card.Content>
-                  <Card.Header>Matthew Harris</Card.Header>
-                  <Card.Meta>Co-Worker</Card.Meta>
-                  <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
-                </Card.Content>
-              </Card>
-              <Card>
-                <Card.Content>
-                  <Card.Header>You</Card.Header>
-                  <Card.Meta>Co-Worker</Card.Meta>
-                  <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
-                </Card.Content>
-              </Card>
-            </Card.Group>
-          </div>
-        </Container>
+        <div>
+          <Card.Group>
+            <Grid columns={3} centered>
+              <Grid.Row>
+                <Card raised>
+                  <Segment inverted color='teal'>
+                    <Card.Content>
+                      <Card.Header>Katherine Piniol</Card.Header>
+                      <Card.Meta>Monday 12:32 pm</Card.Meta>
+                      <Card.Description>
+                        Hello, my name is Katherine Piniol, and I was interested in the lamp you had up for sale.
+                      </Card.Description>
+                    </Card.Content>
+                  </Segment>
+                </Card>
+              </Grid.Row>
+              <Grid.Row>
+                <Card>
+                  <Segment inverted color='olive'>
+                    <Card.Content>
+                      <Card.Header>You</Card.Header>
+                      <Card.Meta>Monday 3:54 pm</Card.Meta>
+                      <Card.Description>Yes, hi! The listed price was $15.62, does that work for you?</Card.Description>
+                    </Card.Content>
+                  </Segment>
+                </Card>
+              </Grid.Row>
+              <Grid.Row>
+                <Card>
+                  <Segment inverted color='teal'>
+                    <Card.Content>
+                      <Card.Header>Katherine Piniol</Card.Header>
+                      <Card.Meta>Monday 7:20 pm</Card.Meta>
+                      <Card.Description>It's works just fine. Shall we meet at Campus Center on Friday at
+                        3?</Card.Description>
+                    </Card.Content>
+                  </Segment>
+                </Card>
+              </Grid.Row>
+              <Grid.Row>
+                <Card>
+                  <Segment inverted color='olive'>
+                    <Card.Content>
+                      <Card.Header>You</Card.Header>
+                      <Card.Meta>Tuesday 9:54 am</Card.Meta>
+                      <Card.Description>Sounds good!</Card.Description>
+                    </Card.Content>
+                  </Segment>
+                </Card>
+              </Grid.Row>
+              <Grid.Row>
+                <Form reply>
+                  <label>Input message:</label>
+                  <Form.TextArea/>
+                  <Button content='Reply' labelPosition='left'/>
+                </Form>
+              </Grid.Row>
+            </Grid>
+          </Card.Group>
+        </div>
     )
   }
 }
@@ -48,8 +91,19 @@ class Messagepage extends React.Component {
   render() {
     return (
         <div>
-          <MessageHead/>
-          <MessageFeed/>
+          <Container>
+            <Grid centered columns={3}>
+              <Grid.Row centered>
+                <Grid.Column>
+                  <div className='messagebgpage'>
+                    <MessageHead/>
+                    <Divider hidden/>
+                    <MessageFeed/>
+                  </div>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
         </div>
     );
   }
