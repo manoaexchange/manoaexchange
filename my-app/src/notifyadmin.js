84b173ed63af7c4c1d0598d2c83e81daf6f9e772
chrisn3 <35389960+chrisn3@users.noreply.github.com>
2019-04-08 22:47:43 -1000

    Added container

--- my-app/src/notifyadmin.js
+++ my-app/src/notifyadmin.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Form, Input, TextArea, Button, Select } from 'semantic-ui-react'
+import { Form, Input, TextArea, Button, Select, Container } from 'semantic-ui-react'
 
 const issueOptions = [
   { key: 'b', text: 'Report a bug', value: 'nug' },
@@ -9,42 +9,44 @@ const issueOptions = [
 ];
 
 const Notify = () => (
-    <Form>
-      <Form.Group widths='equal'>
+    <Container>
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Field
+              id='form-input-control-first-name'
+              control={Input}
+              label='First name'
+              placeholder='First name'
+          />
+          <Form.Field
+              id='form-input-control-last-name'
+              control={Input}
+              label='Last name'
+              placeholder='Last name'
+          />
+          <Form.Field
+              control={Select}
+              options={issueOptions}
+              label={{ children: 'Issue', htmlFor: 'form-select-control-gender' }}
+              placeholder='Select'
+              search
+              searchInput={{ id: 'form-select-control-gender' }}
+          />
+        </Form.Group>
         <Form.Field
-            id='form-input-control-first-name'
-            control={Input}
-            label='First name'
-            placeholder='First name'
+            id='form-textarea-control-opinion'
+            control={TextArea}
+            label='Comments'
+            placeholder='Any other issues here'
         />
         <Form.Field
-            id='form-input-control-last-name'
-            control={Input}
-            label='Last name'
-            placeholder='Last name'
+            id='form-button-control-public'
+            control={Button}
+            content='Confirm'
+            label='Submit'
         />
-        <Form.Field
-            control={Select}
-            options={issueOptions}
-            label={{ children: 'Issue', htmlFor: 'form-select-control-gender' }}
-            placeholder='Select'
-            search
-            searchInput={{ id: 'form-select-control-gender' }}
-        />
-      </Form.Group>
-      <Form.Field
-          id='form-textarea-control-opinion'
-          control={TextArea}
-          label='Comments'
-          placeholder='Any other issues here'
-      />
-      <Form.Field
-          id='form-button-control-public'
-          control={Button}
-          content='Confirm'
-          label='Submit'
-      />
-    </Form>
+      </Form>
+    </Container>
 )
 
 export default Notify
