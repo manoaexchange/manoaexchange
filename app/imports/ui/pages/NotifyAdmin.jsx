2dfcae25da3306fe19163f9625822e433dcb403a
Katherine Piniol <piniolk@hawaii.edu>
2019-04-10 18:42:49 -1000

    remove unnecessary lines

--- app/imports/ui/pages/NotifyAdmin.jsx
+++ app/imports/ui/pages/NotifyAdmin.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import { Stuffs, StuffSchema } from '/imports/api/stuff/stuff';
-import { Grid, Segment, Header } from 'semantic-ui-react';
-import AutoForm from 'uniforms-semantic/AutoForm';
-import TextField from 'uniforms-semantic/TextField';
-import NumField from 'uniforms-semantic/NumField';
-import SelectField from 'uniforms-semantic/SelectField';
-import SubmitField from 'uniforms-semantic/SubmitField';
-import HiddenField from 'uniforms-semantic/HiddenField';
-import ErrorsField from 'uniforms-semantic/ErrorsField';
+import { Stuffs } from '/imports/api/stuff/stuff';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { Meteor } from 'meteor/meteor';
-import { Form, Input, TextArea, Button, Select, Container } from 'semantic-ui-react'
+import { Form, Input, TextArea, Button, Select, Container } from 'semantic-ui-react';
 
 const issueOptions = [
   { key: 'b', text: 'Report a bug', value: 'nug' },
