9e981d7e0cad8a07386de983cc6f2dea1cbd4bb8
SAnthony08 <42788145+SAnthony08@users.noreply.github.com>
2019-04-13 21:54:45 -1000

    Update CategoryList.jsx

--- app/imports/ui/pages/CategoryList.jsx
+++ app/imports/ui/pages/CategoryList.jsx
@@ -21,12 +21,12 @@ class CategoryList extends React.Component {
           <div className='CategoriesPagesBox listSearchBox fauxBoxShadow'>
             <Card.Group centered>
               <Card>
-                <Card.Content>
+                <Card.Content align="center">
                   <Card.Header>Desk Lamp</Card.Header>
                   <Image size='small'
                          src='https://www.publicdomainpictures.net/pictures/200000/nahled/desk-lamp-1475958733bLG.jpg'/>
                 </Card.Content>
-                <Card.Content extra>
+                <Card.Content extra align="center">
                   <Button basic color='black'>
                     See Product
                   </Button>
