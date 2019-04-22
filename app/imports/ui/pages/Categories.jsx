ba4892c4db70833785ed407a3153c7727068afd3
Katherine Piniol <piniolk@hawaii.edu>
2019-04-21 18:57:31 -1000

    added backend for categories and profilepage pages

--- app/imports/ui/pages/Categories.jsx
+++ app/imports/ui/pages/Categories.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Grid, Container, Card } from 'semantic-ui-react';
+import { Grid, Container, Card, Loader } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
+import { Stuffs } from '/imports/api/stuff/stuff';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
 
 class Categories extends React.Component {
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  renderPage() {
     return (
         <div>
           <Container>
@@ -48,4 +56,18 @@ class Categories extends React.Component {
   }
 }
 
-export default Categories;
+/** Require an array of Stuff documents in the props. */
+Categories.propTypes = {
+  stuffs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe('Stuff');
+  return {
+    stuffs: Stuffs.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(Categories);
--- app/imports/ui/pages/Profilepage.jsx
+++ app/imports/ui/pages/Profilepage.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Grid, Segment, Container, Divider } from 'semantic-ui-react';
+import { Grid, Segment, Container, Divider, Loader } from 'semantic-ui-react';
 import ProfileProductsOffered from '../components/ProfileProductsOffered';
 import ProfileDescription from '../components/ProfileDescription';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+import { Stuffs } from '/imports/api/stuff/stuff';
 
 class Profilepage extends React.Component {
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  renderPage() {
     return (
         <div>
           <Grid centered columns={3}>
             <Segment className='profile desc box'>
-                <Container>
-                  <div>
-                    <h1>Profile Page</h1>
-                    <ProfileDescription/>
-                    <Divider inverted/>
-                    <ProfileProductsOffered/>
-                  </div>
-                </Container>
+              <Container>
+                <div>
+                  <h1>Profile Page</h1>
+                  <ProfileDescription/>
+                  <Divider inverted/>
+                  <ProfileProductsOffered/>
+                </div>
+              </Container>
             </Segment>
           </Grid>
         </div>
@@ -24,4 +32,18 @@ class Profilepage extends React.Component {
   }
 }
 
-export default Profilepage;
+/** Require an array of Stuff documents in the props. */
+Profilepage.propTypes = {
+  stuffs: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe('Stuff');
+  return {
+    stuffs: Stuffs.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(Profilepage);
