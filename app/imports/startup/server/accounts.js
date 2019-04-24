4be9f0429575e422fc6a0f7d796daccb45d67e76
SAnthony08 <42788145+SAnthony08@users.noreply.github.com>
2019-04-23 15:50:01 -1000

    Implemented profile object in accounts

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -4,12 +4,15 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
-function createUser(name, email, password, role) {
-  console.log(`  Creating user ${profile.name}.`);
+function createUser(email, password, profile, role) {
+  console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
-    email: email,
+    username: email,
     password: password,
-    profile: Object,
+    profile: {
+      name:  profile.name,
+      flagged: profile.flagged,
+    }
   });
   if (role === 'admin') {
     Roles.addUsersToRoles(userID, 'admin');
@@ -20,7 +23,7 @@ function createUser(name, email, password, role) {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.map(({ email, password, profile, role }) => createUser(email, password, profile, role));
+    Meteor.settings.defaultAccounts.map(({ username, password, profile, role }) => createUser(username, password, profile, role));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
