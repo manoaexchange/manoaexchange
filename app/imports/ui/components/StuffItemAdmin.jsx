5b924be1e4a663c752fb0ec71645c56a0fdbaa9b
chrisn3 <35389960+chrisn3@users.noreply.github.com>
2019-04-24 16:07:25 -1000

    Edited for admin capabilities

--- app/imports/ui/components/StuffItemAdmin.jsx
+++ app/imports/ui/components/StuffItemAdmin.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import { withRouter, Link } from 'react-router-dom';
 
-/** Renders a single row in the List Stuff (Admin) table. See pages/ListStuffAdmin.jsx. */
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class StuffItemAdmin extends React.Component {
   render() {
     return (
         <Table.Row>
-          <Table.Cell>{this.props.stuff.name}</Table.Cell>
-          <Table.Cell>{this.props.stuff.quantity}</Table.Cell>
-          <Table.Cell>{this.props.stuff.condition}</Table.Cell>
-          <Table.Cell>{this.props.stuff.owner}</Table.Cell>
+          <Table.Cell>{this.props.items.item}</Table.Cell>
+          <Table.Cell>{this.props.items.quantity}</Table.Cell>
+          <Table.Cell>{this.props.items.description}</Table.Cell>
+          <Table.Cell>{this.props.items.image}</Table.Cell>
+          <Table.Cell>{this.props.items.condition}</Table.Cell>
+          <Table.Cell>{this.props.items.owner}</Table.Cell>
+          <Table.Cell>
+            <Link to={`/edit/${this.props.items._id}`}>Edit</Link>
+          </Table.Cell>
         </Table.Row>
     );
   }
@@ -18,7 +24,8 @@ class StuffItemAdmin extends React.Component {
 
 /** Require a document to be passed to this component. */
 StuffItemAdmin.propTypes = {
-  stuff: PropTypes.object.isRequired,
+  items: PropTypes.object.isRequired,
 };
 
-export default StuffItemAdmin;
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(StuffItemAdmin);
