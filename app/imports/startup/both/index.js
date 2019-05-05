c7867d74526cb807baefea19a3d975b72be3dd6f
chrisn3 <35389960+chrisn3@users.noreply.github.com>
2019-05-04 21:12:23 -1000

    Added a categories collection

--- app/imports/startup/both/index.js
+++ app/imports/startup/both/index.js
@@ -1,3 +1,4 @@
 import '/imports/api/stuff';
 import '/imports/api/profile';
 import '/imports/api/reports';
+import '/imports/api/categories';
--- app/imports/startup/server/index.js
+++ app/imports/startup/server/index.js
@@ -2,3 +2,4 @@ import './accounts.js';
 import './items.js';
 import './profile.js';
 import './reports.js';
+import './categories.js';
